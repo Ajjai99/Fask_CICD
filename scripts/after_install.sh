02f7b10c76d0ad10e9119ffd29126cbfccdb6a02
Ajjai99 <ajjaimannan1999@gmail.com>
2024-02-07 12:58:25 +0530

    new changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,5 +5,5 @@ docker login --username AWS --password $(aws ecr get-login-password --region us-
 echo "Logged in to Amazon ECR successfully."
 
 echo "Pulling image from Amazon ECR"
-docker pull 761823580113.dkr.ecr.us-west-2.amazonaws.com/flask_image:latest
+docker pull 761823580113.dkr.ecr.us-west-2.amazonaws.com/flask_image:0.1
 echo "Pulled image from Amazon ECR successfully."
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,2 @@
 echo "Running container..."
-docker run --name flask_app -d -p 5000:5000 761823580113.dkr.ecr.us-west-2.amazonaws.com/flask_image:latest
+docker run --name flask_app -d -p 5000:5000 761823580113.dkr.ecr.us-west-2.amazonaws.com/flask_image:0.1
